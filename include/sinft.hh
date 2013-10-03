856035e8c352cd73e1b50bfbff5f84673aa75261
Arturo Apu Chinchilla <arturoapu@gmail.com>
2013-10-03 17:11:34 -0600

    all I can say its that my life is pretty plane

--- include/sinft.hh
+++ include/sinft.hh
@@ -1,3 +1,6 @@
-void sinft (double *y, const int size);
+#include <complex>
+
+void sinft (double *y, std::complex<double> *rbuffer, const int size);
+
 int get_array_lenght (double *arr, const int size);
 void complete_arr (double *arr_in, double *arr_out, const int size);
--- src/sinft.cpp
+++ src/sinft.cpp
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <iostream>
 #include <math.h>
+#include <complex>
+
+#include <fftwest.hh>
+
+using namespace std;	
 
 int get_array_lenght (double *arr, const int size) {	
 	int n,array_lenght=0;
@@ -26,79 +31,7 @@ void complete_arr (double *arr_in, double *arr_out, const int size) {
 	}	
 }
 
-void four1(double *data, const int n, const int isign) {
-	int nn,mmax,m,j,istep,i;
-	double wtemp,wr,wpr,wpi,wi,theta,tempr,tempi;
-	if (n<2 || n&(n-1)) throw("n must be power of 2 in four1");
-	nn = n << 1;
-	j = 1;
-	for (i=1;i<nn;i+=2) {
-		if (j > i) {
-			std::swap(data[j-1],data[i-1]);
-			std::swap(data[j],data[i]);
-		}
-		m=n;
-		while (m >= 2 && j > m) {
-			j -= m;
-			m >>= 1;
-		}
-		j += m;	
-	}
-	mmax=2;
-	while (nn > mmax) {
-		istep=mmax << 1;
-		theta=isign*(6.2831/mmax);
-		wtemp=sin(0.5*theta);
-		wpr = -2.0*wtemp*wtemp;
-		wpi=sin(theta);
-		wr=1.0;
-		wi=0.0;
-		for (m=1;m<mmax;m+=2) {
-			for (i=m;i<=nn;i+=istep) {
-				j=i+mmax;
-				tempr=wr*data[j-1]-wi*data[j];
-				tempi=wr*data[j]+wi*data[j-1];
-				data[j-1]=data[i-1]-tempr;
-				data[j]=data[i]-tempi;
-				data[i-1] += tempr;
-				data[i] += tempi;
-			}
-			wr=(wtemp=wr)*wpr-wi*wpi+wr;
-			wi=wi*wpr+wtemp*wpi+wi;
-		}
-	mmax=istep;
-	}
-}
-
-void realft(double *data, const int size) {
-	int i,i1,i2,i3,i4,n=size;
-	double c1=0.5,c2=0.5,h1r,h1i,h2r,h2i,wr,wi,wpr,wpi,wtemp;
-	double theta=-3.1415/double(n>>1); 
-	
-	wtemp=sin(0.5*theta);
-	wpr = -2.0*wtemp*wtemp;
-	wpi=sin(theta);
-	wr=1.0+wpr;
-	wi=wpi;
-	for (i=1;i<(n>>2);i++) {
-		i2=1+(i1=i+i);
-		i4=1+(i3=n-i1);
-		h1r=c1*(data[i1]+data[i3]);
-		h1i=c1*(data[i2]-data[i4]);
-		h2r= -c2*(data[i2]+data[i4]);
-		h2i=c2*(data[i1]-data[i3]);
-		data[i1]=h1r+wr*h2r-wi*h2i;
-		data[i2]=h1i+wr*h2i+wi*h2r;
-		data[i3]=h1r-wr*h2r+wi*h2i;
-		data[i4]= -h1i+wr*h2i+wi*h2r;
-		wr=(wtemp=wr)*wpr-wi*wpi+wr;
-		wi=wi*wpr+wtemp*wpi+wi;
-	}
-	data[0] = (h1r=data[0])+data[1];
-	data[1] = h1r-data[1];
-}
-
-void sinft (double *y, const int size){
+void sinft (double *y, complex<double> *rbuffer, const int size){
 	int j,n=size;
 	double sum,y1,y2,theta,wi=0.0,wr=1.0,wpi,wpr,wtemp;
 	theta=3.1415/double(n);
@@ -116,12 +49,13 @@ void sinft (double *y, const int size){
 		y[j]=y1+y2;
 		y[n-j]=y1-y2;
 	}
-	realft(y,size);
-	y[0]*=0.5;
-	sum=y[1]=0.0;
-	for (j=0;j<n-1;j+=2) {
-		sum += y[j];
-		y[j]=y[j+1];
-		y[j+1]=sum;
+	//complex<double> *retbuffer = (complex<double>*) malloc((0.5*n+1)*sizeof(complex<double>));
+	fft_west(size, y, rbuffer);
+	
+	rbuffer[0] = complex<double> (0.5*real(rbuffer[j]),0);
+
+	for (j=0;j<(n-1)/2;j+=2) {
+		sum += imag(rbuffer[j]);
+		rbuffer[j] = complex<double> (imag(rbuffer[j]),sum);
 	}
 }
